a59e1186a1257f6b4bb030ee4bc69caffab20965
Tugba A <karaca.tgb@gmail.com>
2020-12-21 18:38:11 +0100

    UserPrincipal class added

---
+++ src/main/java/com/example/demo/auth/UserFlxPrincipal.java
@@ -0,0 +1,54 @@
+package com.example.demo.auth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class UserFlxPrincipal implements UserDetails {
+
+    private UserFlx userFlx;
+
+    public UserFlxPrincipal(UserFlx userFlx) {
+        super();
+        this.userFlx = userFlx;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.userFlx.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userFlx.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
