966cb34f692fb01f27ce95d6d58b3a9cf0db2408
ha49 <kaysersoza@outlook.com>
2021-01-16 18:51:52 +0100

    adding jms

--- src/main/java/com/example/demo/auth/UserFlxRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.auth;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserFlxRepository extends CrudRepository<UserFlx, Long> {
-    UserFlx findByUsername(String username);
-
-
-}
