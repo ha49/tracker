4ccb14ed892306e6de312803a3171279ad660b1b
ha <kaysersoza@outlook.com>
2021-05-31 12:52:52 +0200

    Jwt implementation

---
+++ src/main/java/com/example/tracker/security/jwt/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,4 @@
+package com.example.tracker.security.jwt.config;
+
+public class JwtAuthenticationEntryPoint {
+}
---
+++ src/main/java/com/example/tracker/security/jwt/model/JwtResponse.java
@@ -0,0 +1,17 @@
+package com.example.tracker.security.jwt.model;
+
+public class JwtResponse {
+
+    private final String jwt;
+
+    public JwtResponse(String jwt) {
+
+        this.jwt = jwt;
+    }
+
+    public String getToken() {
+
+        return this.jwt;
+    }
+
+}
